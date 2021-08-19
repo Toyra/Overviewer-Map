5df7fbee5066694b979a89a5018ff60256f4f30c
Leo von Ahn <leotoyra@gmail.com>
2021-08-19 17:52:33 +0200

    auto commit

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1629385235",
+        "cacheTag": "1629388352",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1629382891,
+            "last_rendertime": 1629386488,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
