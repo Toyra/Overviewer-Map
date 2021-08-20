54f85a0e32dd8379005f7f4a4399f61a1b8eee17
Leo von Ahn <leotoyra@gmail.com>
2021-08-20 10:00:31 +0200

    auto commit

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1629442831",
+        "cacheTag": "1629446430",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
