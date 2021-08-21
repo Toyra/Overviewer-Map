0f2cda8e4cfc128a8ec6967ebab34180c07f2bd6
Leo von Ahn <leotoyra@gmail.com>
2021-08-21 11:32:47 +0200

    auto commit

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -28,7 +28,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1629537626",
+        "cacheTag": "1629538366",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -50,7 +50,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "Toyboys - overworld",
-            "last_rendertime": 1629537383,
+            "last_rendertime": 1629537983,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
