3979fd45033161ef193f4ad926431c289361e2b9
Leo von Ahn <leotoyra@gmail.com>
2021-08-21 13:25:52 +0200

    force render

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -28,7 +28,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1629543509",
+        "cacheTag": "1629545060",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
