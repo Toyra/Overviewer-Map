5ac8f9da83b1239772cb4b88fc27fcecfc01b3c8
Leo von Ahn <leotoyra@gmail.com>
2021-08-23 20:23:33 +0200

    manual full

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -28,7 +28,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1629741633",
+        "cacheTag": "1629743010",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
