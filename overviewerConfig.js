f236faaf84dc5ad4ae37016605194e4932510c9a
Leo von Ahn <leotoyra@gmail.com>
2021-08-25 15:56:39 +0200

    manual full

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -28,7 +28,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1629896574",
+        "cacheTag": "1629899788",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -50,7 +50,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "Toyboys - overworld",
-            "last_rendertime": 1629896272,
+            "last_rendertime": 1629899272,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
@@ -77,7 +77,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "Toyboys - overworld",
-            "last_rendertime": 1629896272,
+            "last_rendertime": 1629899272,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
