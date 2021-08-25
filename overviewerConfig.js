2d95f6b732edde1e6d20455b3b00b49480bdd76a
Leo von Ahn <leotoyra@gmail.com>
2021-08-25 12:05:54 +0200

    automated full

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -28,7 +28,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1629882096",
+        "cacheTag": "1629885933",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -50,7 +50,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "Toyboys - overworld",
-            "last_rendertime": 1629881968,
+            "last_rendertime": 1629885476,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
@@ -77,7 +77,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "Toyboys - overworld",
-            "last_rendertime": 1629881968,
+            "last_rendertime": 1629885476,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
