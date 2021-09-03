b4e3d0f40f2c892a01f57b899a8fa48df52357c2
Leo von Ahn <leotoyra@gmail.com>
2021-09-03 07:00:35 +0200

    automated full

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -28,7 +28,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1630641634",
+        "cacheTag": "1630645234",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
