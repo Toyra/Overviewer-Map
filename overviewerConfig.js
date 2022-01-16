97809006c33293ba60487ba612f8231eedaeec8b
Leo von Ahn <leotoyra@gmail.com>
2022-01-16 06:00:34 +0100

    automated full

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -28,7 +28,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1642305633",
+        "cacheTag": "1642309233",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
